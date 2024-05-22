025d9fdbffe5781a869fbde6c37dbe8290f9d63f
denise <d189@umbc.edu>
2024-05-22 11:27:06 -0400

    connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
+require("dotenv").config();
+
 const express = require("express");
+const { connect } = require("./db");
 
 const app = express();
 
 const router = require("./routes");
 app.use("/api", router);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`server running on http://localhost:${PORT}`);
-});
+async function startServer() {
+  await connect();
+
+  app.listen(PORT, () => {
+    console.log(`server running on http://localhost:${PORT}`);
+  });
+}
+
+startServer();
