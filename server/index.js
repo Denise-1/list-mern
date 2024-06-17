41319d4dc5e530144abbea17fecfdfec522fb249
denise <d189@umbc.edu>
2024-06-17 11:21:46 -0400

    build folder

--- server/index.js
+++ server/index.js
@@ -3,9 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const { connect } = require("./db");
 
+const path = require("path");
+
 const app = express();
 // middleware
-app.use(express.json())
+app.use(express.json());
+
+//deployment
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname,"build/index.html"))
+})
 
 const router = require("./routes");
 app.use("/api", router);
